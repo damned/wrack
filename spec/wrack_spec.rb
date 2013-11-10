0d8b6b57e50a71d07fb3a5c6449bd8d92097e4d3
danm <writetodan@yahoo.com>
2013-11-10 16:55:56 +0000

    @damned fixed website spawn

--- spec/wrack_spec.rb
+++ spec/wrack_spec.rb
@@ -1,17 +1,35 @@
 require 'rspec'
 require 'net/http'
+require 'socket'
 
 describe 'wrack' do
 
-  before :all do
-    @website = Kernel.fork do
-      `rackup`
+  def server_up
+    begin
+      print 'connecting...'
+      TCPSocket.new('localhost', 8080).close
+      puts 'ok'
+      true
+    rescue
+      puts 'barf!'
+      false
     end
   end
 
+  before :all do
+    @website = Kernel.spawn 'rackup'
+    until server_up do
+      sleep 0.2
+    end
+    puts "spawned website pid: #{@website}"
+  end
+
   after :all do
-    Process.kill 'TERM', @website
+    puts "killing website pid: #{@website}"
+    Process.kill 'KILL', @website
+    puts "waiting for website pid: #{@website}"
     Process.wait @website
+    puts "waited for website pid: #{@website}"
   end
 
   it 'should say hello' do
